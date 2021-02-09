701c6c25c9432370562fab59b2a2f5e45d5029d6
avidms <avram.dames@gmail.com>
2021-02-09 12:27:53 +0100

    simple router implemented

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp, h } from 'vue'
+import routes from './routes'
 import "@/assets/styles/main.css";
 
-createApp(App).mount('#app')
+const SimpleRouterApp = {
+    data: () => ({
+      currentRoute: window.location.pathname
+    }),
+  
+    computed: {
+      ViewComponent () {
+        const matchingPage = routes[this.currentRoute] || '404'
+        return require(`./pages/${matchingPage}.vue`).default
+      }
+    },
+  
+    render () {
+      return h(this.ViewComponent)
+    },
+  
+    created () {
+      window.addEventListener('popstate', () => {
+        this.currentRoute = window.location.pathname
+      })
+    }
+  }
+  
+  createApp(SimpleRouterApp).mount('#app')
---
+++ src/pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <main-layout>
+    <h1 class="text-yellow-500">About</h1>
+    <p>{{msg}}</p>
+  </main-layout>
+</template>
+
+<script>
+import MainLayout from '../layouts/Main.vue'
+export default {
+  components: {
+    MainLayout
+  }
+}
+</script>
