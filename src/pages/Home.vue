e20d468ead1ab952b9f287cd2d29399502fb15f2
avidms <avram.dames@gmail.com>
2021-02-10 15:10:57 +0100

    add fonts, navbar and hero

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,56 @@
 <template>
   <main-layout>
-    <p>Welcome home!</p>
+    <div class="container">
+      <div class="h-screen bg-gray-200 md:bg-white md:grid md:grid-cols-2 md:gap-4">
+        <div class="flex items-end md:items-center">
+          <div class="flex flex-col">
+            <h1 class="font-mwb text-2xl md:text-4xl">
+              Zâmbetul perfect începe cu o consultație
+            </h1>
+            <p class="hidden md:inline-block font-mvr text-lg">
+              Misiunea noastră este redarea zâmbetului dumneavoastră, acesta
+              fiind oglinda stării de bine a întregului organism.
+            </p>
+            <div class="mt-5 sm:mt-8 md:flex md:justify-start">
+              <div class="rounded-md shadow">
+                <a
+                  href="#"
+                  class="w-full flex items-center justify-center px-10 py-2 border border-transparent font-mwsr text-sm rounded-md text-white bg-primary hover:bg-primary-light"
+                >
+                  Află cum
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="hidden md:inline-block">
+          <img
+            src="../assets/images/header.png"
+            alt="Photo of smiling lady by Hian Oliveira borrowd from Unsplash"
+          />
+        </div>
+      </div>
+    </div>
+
+    <div class="px-4">
+      <div class="max-w-3xl bg-red-200 rounded-lg mx-auto my-16 p-16">
+        <h1 class="text-2xl font-mwb mb-2">
+          Let's build with TailwindCSS
+        </h1>
+        <h2 class="uppercase tracking-wide">
+          Misiunea noastră este redarea zâmbetului dumneavoastră, acesta
+          fiind oglinda stării de bine a întregului organism.
+        </h2>
+      </div>
+    </div>
   </main-layout>
 </template>
 
 <script>
-import MainLayout from '../layouts/Main.vue'
+import MainLayout from "../layouts/Main.vue";
 export default {
   components: {
-    MainLayout
-  }
-}
+    MainLayout,
+  },
+};
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#D4A45B',
+        'primary-light': "#E3B977",
+        'gray': "#827F7C",
+        'dark': "1C1812"
+      }
+    },
+    container: {
+      center: true,
+    },
+    fontFamily: {
+      mwb : ['Merriweather-Bold', 'serif'],
+      mwsb: ['MerriweatherSans-Bold', 'font-sans'],
+      mwsr: ['MerriweatherSans-Regular', 'font-sans'],
+      mwsl: ['MerriweatherSans-Light', 'font-sans'],
+      mvr: ['MuktaVanni-Regular', 'font-sans'],
+    },
+    backgroundImage: theme => ({
+      'hero-image': "url('../images/hian-oliveira-n_L_ppO4QtY-unsplash.jpg')",
+      'hero-none': "#FFFFFF"
+    })
   },
   variants: {
     extend: {},
